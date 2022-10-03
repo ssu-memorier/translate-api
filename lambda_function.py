cd9a6d4377bc7c66423315c271e06b8db302bde1
Jiwonii97 <wldnjs3633@naver.com>
2022-10-03 23:51:44 +0900

    기본 Handler functoin 구현

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+import getGoogleTrans
+import getPapagoTrans
+
+
+def create_papago_return(text1, text2):
+    return {
+        "source": "en",
+        "target": "ko",
+        "text": {
+            "origin": text1,
+            "translated": text2
+        }
+    }
+
+
+def lambda_handler(event, context):
+    print(event)
+    print(context)
+    msg = event['message']
+
+    translated_text = getPapagoTrans.get_translate(msg)
+    # translated_text = get_google_trans(msg)
+
+    return {
+        'statusCode': 200,
+        'body': create_papago_return(msg, translated_text)
+    }
