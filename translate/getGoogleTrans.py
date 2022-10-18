fd158b3153d8b2ae0bac30bb30dfd5df03709ac3
Jiwonii97 <wldnjs3633@naver.com>
2022-10-18 21:41:54 +0900

    Google-Trans API 추가

--- translate/getGoogleTrans.py
+++ translate/getGoogleTrans.py
@@ -1,6 +1,35 @@
-# def get_google_trans(texts):
+from googletrans import Translator
 
-#     translator = Translator()
-#     res = translator.translate(texts, dest="ko").text
 
-#     return res
+def get_translate(texts, src, tgt):
+
+    translator = Translator()
+    text = translator.translate(texts, src=src, dest=tgt).text
+    extra_data = translator.translate(texts, src=src, dest=tgt).extra_data
+
+    # 번역 결과로 이미 주어져서 사용안해도 될듯 X
+
+    if extra_data["all-translations"] is not None:
+        # 모든 번역 결과
+        allTranslations = {}
+        for translations in extra_data["all-translations"]:
+            wordType = translations[0]
+            possibleTranslations = translations[1]
+
+            allTranslations[wordType] = possibleTranslations
+    else:
+        allTranslations = None
+
+    return create_response_return(texts, text, src, tgt, allTranslations)
+
+
+def create_response_return(orgText, translatedText, src, tgt, allTranslations):
+    return {
+        "source": src,
+        "target": tgt,
+        "text": {
+            "origin": orgText,
+            "translated": translatedText
+        },
+        "allTranslations": allTranslations,
+    }
