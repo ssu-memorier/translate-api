46a1c88e0ae6496ed28fcf753b37a57267085e8e
Jiwonii97 <wldnjs3633@naver.com>
2022-11-01 21:32:34 +0900

    변수명 수정

--- restApi/views.py
+++ restApi/views.py
@@ -9,16 +9,16 @@ from translate import googleTrans
 
 @api_view(["POST"])
 def googleTranslate(request):
-    user_serializer = RequestSerializer(data=request.data)
+    transSerializer = RequestSerializer(data=request.data)
 
-    if user_serializer.is_valid():
+    if transSerializer.is_valid():
         try:
-            msg, src, tgt = user_serializer.data.values()
+            msg, src, tgt = transSerializer.data.values()
             result = googleTrans.get_translate(msg, src, tgt)
 
             return Response(result, status=status.HTTP_200_OK)
         except ValueError:
-            return Response(user_serializer.error_messages["invalid"], status=status.HTTP_400_BAD_REQUEST)
+            return Response(transSerializer.error_messages["invalid"], status=status.HTTP_400_BAD_REQUEST)
 
     else:
-        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(transSerializer.error_messages["invalid"], status=status.HTTP_400_BAD_REQUEST)
