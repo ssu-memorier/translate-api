fb3a814e70e91f418a679d3aa88744aadaa11a5f
Jiwonii97 <wldnjs3633@naver.com>
2022-11-01 18:26:36 +0900

    모델 삭제 및 시리얼라이즈 내용 수정

--- restApi/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Request(models.Model):
-    message = models.CharField(max_length=10000, null=False)
-    source = models.CharField(max_length=128, null=False)
-    target = models.CharField(max_length=128, null=False)
--- restApi/serializers.py
+++ restApi/serializers.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers
-from .models import Request
-
-'''
-    django REST framework에서 제공하는 serializer를 활용하여
-    request로 받은 data를 역직렬화 하여 DB에 반영하고
-    reponse로 사용 될 data를 다시 직렬화하여 json이나 xml등으로 손쉽게 변환이 가능하다.
-'''
 
 
-class RequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Request
-        fields = '__all__'
-        # 모델 Request의 모든 field를 serializer함.
+class RequestSerializer(serializers.Serializer):
+    message = serializers.CharField(max_length=10000)
+    source = serializers.CharField(max_length=128)
+    target = serializers.CharField(max_length=128)
